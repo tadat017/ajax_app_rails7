c1e27dd1cc3e2e99dd7af61af1d5c12ac10106c3
tadat017 <tadat017@gmail.com>
2024-06-03 19:54:49 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,20 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:content])
+    
+    post = Post.create(content: params[:content])
+    #新たに投稿されたメモの内容を変数postに格納
     redirect_to action: :index
-  end
+     #redirect_toを用いることでindex.html.erbを返す
+
+     render json:{ post: post }
+     #renderメソッドを用いて、レスポンスで返却されるデータフォーマット
+     #にJSONを指定
+     #前列で定義した変数postの値をpostというキーとセットで
+     #JavaScriptに送信している
+
+    end
 end
+
+#レスポンスでindex.html.erbを返すように指定しているのは、
+#posts_controller.rbのcreateアクション
